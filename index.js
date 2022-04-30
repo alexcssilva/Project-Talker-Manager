5b6f1d3258a956c40c50167cd8e29457102e515b
Alex Silva <alexcssilva@gmail.com>
2022-04-30 19:15:43 -0300

    Desafio 02 concluido

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ app.listen(PORT, () => {
 app.get('/talker', (req, res) => {
   const data = fs.readFileSync(fileTalker, 'utf8');
 
-    res.status(200).json(JSON.parse(data));
+    res.status(HTTP_OK_STATUS).json(JSON.parse(data));
     console.log(data);
+});
+
+// 2 - Crie o endpoint GET /talker/:id
+// O endpoint deve retornar uma pessoa palestrante com base no id da rota. Devendo retornar o status 200 ao fazer uma requisição /talker/1, com o seguinte corpo:
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync(fileTalker, 'utf8');
+
+  const fileContent = JSON.parse(data);
+
+  const chosenId = fileContent.find((params) => params.id === parseInt(id, 0));
+
+  if (chosenId === undefined) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+    return res.status(HTTP_OK_STATUS).json(chosenId);
 });
